704929753acd3b14000f2a958279d4a77aa91426
romanidzs <romanidzs@gmail.com>
2023-03-02 10:40:57 +0700

    Add subprocess dict

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from typing import Union
 import asyncio
 import logging as log
 from fastapi import FastAPI, Body
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 log.basicConfig(level=log.INFO, filename="log.log", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
 log.info("Start server")
-flag = 0
 PYTHON_PATH = 'C:/Users/Роман/PycharmProjects/Server_D/venv/Scripts/python.exe'
+status = dict()
 
 
 @app.get("/")
@@ -26,8 +27,12 @@ def ping():
 @app.post("/api/v1/analysis/start")
 def start_analysis():
     log.info("Post start analysis")
+    s = status.get('1')
+    if s is None:
+        return {"Message": "На данный момент у данного пользователя уже запущен процесс обучения."}
     sp = subprocess.Popen(
         [PYTHON_PATH, os.path.join('.', 'logic.py')])
+    status.update({'1': sp})
     print("Основной поток")
     if sp.stderr is not None:
         return {"Error": sp.stderr}
@@ -37,10 +42,21 @@ def start_analysis():
 @app.get("/api/v1/analysis/status")
 def status_analysis():
     log.info("Get status analysis")
-    if os.path.isfile('./ss.csv'):
+    s = status.get('1')
+    if s is None:
+        return JSONResponse(
+            content={"Message": "На данный момент у данного пользователя нет запущенных процессов анализа."},
+            status_code=404)
+    return_code = s.poll()  # Получение информации о статусе подпроцесса. Завершен, в процессе, прерван.
+    print("Status: ", return_code)
+    # if os.path.isfile('./ss.csv'):
+    if return_code == 0:
+        status.pop('1')
         return {"Status": "Finish"}
-    else:
+    elif return_code is None:
         return {"Status": "processing"}
+    else:
+        return {"Error": s.stderr, "Message": "Error analysis alg"}
 
 
 @app.get("/api/v1/analysis/get-result")
