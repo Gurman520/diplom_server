a6036a7d81e34ec3f4e30b9c2c890383d14b0b0d
romanidzs <romanidzs@gmail.com>
2023-07-29 15:11:07 +0700

    Fix config

--- cm/config.py
+++ cm/config.py
@@ -7,6 +7,6 @@ class Config(object):
     DB_NAME = getenv('DB_NAME', default='server')
     DB_USER = getenv('DB_USER', default='admin')
     DB_PASSWORD = getenv('DB_PASSWORD', default='pgpwd4habr')
-    PYTHON_PATH = getenv('PYTHON_PATH', default='C:/Users/Роман/PycharmProjects/Server_D/venv/Scripts/python.exe')
+    PYTHON_PATH = getenv('PYTHON_PATH', default='usr/bin/python3')
     NAME_FILE_PREDICT = getenv('FILE_PREDICT', default='Files/WorkFiles/get_predict.py')
     NAME_FILE_TRAIN = getenv('FILE_TRAIN', default='Files/WorkFiles/training.py')
